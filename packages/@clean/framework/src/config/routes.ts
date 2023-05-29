6904507c533834dbdcf40cb74d201fa80134e33d
Caxvalencia <caxvalencia@gmail.com>
2023-05-28 23:18:26 -0500

    feat: add @clean/framework module

---
+++ packages/@clean/framework/src/config/routes.ts
@@ -0,0 +1,7 @@
+export default [
+  {
+    method: 'GET',
+    path: '/users/:id',
+    // controller: { className: IndexController, action: 'index' },
+  },
+];
---
+++ packages/@clean/framework/src/facades/route.facade.ts
@@ -0,0 +1,27 @@
+import { Router, Request, HttpMethod } from '@clean/router';
+
+const router = new Router();
+
+export abstract class Route {
+  public static get(
+    path: string,
+    callback: (req: Request, res: Response, ...args: any) => void
+  ) {
+    router.add(HttpMethod.GET, path, callback);
+  }
+
+  public static put(
+    path: string,
+    callback: (req: Request, res: Response, ...args: any) => void
+  ) {
+    router.add(HttpMethod.PUT, path, callback);
+  }
+
+  public static handle(request: Request, response: any) {
+    router.handle(request, response);
+  }
+
+  public static list() {
+    return router.routes;
+  }
+}
